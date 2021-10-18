acde114ce4b6de1ccf9ffdf599ad37441bf91180
Muhib_ur_Rehman <mrehman@nisum.com>
2021-10-18 16:21:12 +0500

    Initial Commit

---
+++ src/test/java/com/example/GithubActionsPractice/GithubActionsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GithubActionsPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
